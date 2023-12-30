f62be23002cf7aef35c024d70c2a2c51eadd9b6e
shunjid <shunjid.rahman@gmail.com>
2023-12-30 22:31:55 +0600

    Refer spilt components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import { GlobeIcon } from "lucide-react";
 import { Metadata } from "next";
 
 import { CommandMenu } from "@/components/command-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { Section } from "@/components/ui/section";
 import { resume } from "@/data";
 
 import Skills from "./skills";
+import Summary from "./summary";
+import Top from "./top";
 
 export const metadata: Metadata = {
   title: `${resume.name}`,
@@ -18,52 +16,9 @@ export default function Page() {
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
-        <div className="flex items-center justify-between">
-          <div className="flex-1 space-y-1.5">
-            <h1 className="text-2xl font-bold">{resume.name}</h1>
-            <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground">
-              {resume.about}
-            </p>
-            <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
-              <a
-                className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
-                href={resume.locationLink}
-                target="_blank"
-                aria-label={resume.location}
-              >
-                <GlobeIcon className="h-3 w-3" />
-                {resume.location}
-              </a>
-            </p>
-            <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
-              {resume.social.map((social) => (
-                <Button
-                  key={social.name}
-                  className="h-8 w-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
-                  <a href={social.url} aria-label={social.name}>
-                    <social.icon className="h-4 w-4" />
-                  </a>
-                </Button>
-              ))}
-            </div>
-          </div>
-
-          <Avatar className="h-28 w-28">
-            <AvatarImage alt={resume.name} src={resume.avatarUrl} />
-            <AvatarFallback>{resume.initials}</AvatarFallback>
-          </Avatar>
-        </div>
-        <Section>
-          <h2 className="text-xl font-bold">About</h2>
-          <p className="text-pretty font-mono text-sm text-muted-foreground">
-            {resume.summaries.join(" ")}
-          </p>
-        </Section>
-        <Skills data={resume.skills} />
+        <Top data={{ ...resume }} />
+        <Summary data={{ ...resume }} />
+        <Skills data={{ ...resume }} />
       </section>
 
       <CommandMenu
