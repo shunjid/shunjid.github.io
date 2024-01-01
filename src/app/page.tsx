ff2f35ca8787de4db6dce61e9686644ef7ce3cb8
shunjid <shunjid.rahman@gmail.com>
2024-01-01 22:17:40 +0600

    Reorder summary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,9 @@ export default function Page() {
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
         <Top data={{ ...resume }} />
+        <Summary data={{ ...resume }} />
         <Experience data={{ ...resume }} />
         <Achievements data={{ ...resume }} />
-        <Summary data={{ ...resume }} />
         <Skills data={{ ...resume }} />
       </section>
 
