fabc70c8253c5238fd8f08a97f9cceb85d5afa69
shunjid <shunjid.rahman@gmail.com>
2024-01-02 23:00:41 +0600

    Add padding bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
-      <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
+      <section className="mx-auto w-full max-w-2xl space-y-8 bg-white pb-8 print:space-y-6">
         <Top data={{ ...resume }} />
         <Summary data={{ ...resume }} />
         <Experience data={{ ...resume }} />
