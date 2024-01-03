9c8d178a9fc21e8a61576c64b0179c400183bb45
shunjid <shunjid.rahman@gmail.com>
2024-01-03 22:57:17 +0600

    Update badges

--- src/app/skills/index.tsx
+++ src/app/skills/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { Badge } from "@/components/ui/badge";
 import { Section } from "@/components/ui/section";
 import { IResume } from "@/data/types";
 
@@ -14,7 +13,14 @@ export default function Skills(
       <h2 className="text-xl font-bold">Skills</h2>
       <div className="flex flex-wrap gap-1">
         {skills.map((skill) => {
-          return <Badge key={skill}>{skill}</Badge>;
+          return (
+            <span
+              key={skill}
+              className="text-primary-700 inline-block whitespace-nowrap rounded-[0.27rem] bg-sky-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none"
+            >
+              {skill}
+            </span>
+          );
         })}
       </div>
     </Section>
